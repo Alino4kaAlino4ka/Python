7f4cd2453953dcb1513997546ded28fc6f224249
Alino4kaAlino4ka <116315690+Alino4kaAlino4ka@users.noreply.github.com>
2022-12-13 15:28:22 +0300

    Update main.py

--- 009/Лекция 5/Лекция 5/main.py
+++ 009/Лекция 5/Лекция 5/main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from bot_commands import *
 
-updater = Updater('5509668401:AAHILPtzjfmvskjXByAO7Pf6_hEASzzyeS4')
+updater = Updater('TOKEN')
 
 updater.dispatcher.add_handler(CommandHandler('hi', hi_command))
 updater.dispatcher.add_handler(CommandHandler('time', time_command))
